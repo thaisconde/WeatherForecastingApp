061d4225b9b2ad349c362ff524f40feed5920982
Thais Mota <75498159+tcmota@users.noreply.github.com>
2021-06-03 20:46:57 -0300

    Update Forecast response

--- WeatherForecastingApp/Model/ForecastWeatherResponse.swift
+++ WeatherForecastingApp/Model/ForecastWeatherResponse.swift
@@ -1,10 +1,19 @@
 struct ForecastWeatherResponse: Decodable {
+    let list: [List]
+    let city: City
+}
+
+struct City: Decodable {
+    let name: String
+    let country: String
+}
+
+struct List: Decodable {
     let date: String?
-    let main: [Main]
+    let main: Main
     let weather: [Weather]
-    
+
     enum codingKey: String, CodingKey {
         case date = "dt_txt"
     }
 }
-
